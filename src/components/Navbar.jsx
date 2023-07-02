d074d3aea81ea5ddd702ef8f879cfc4f94896c9c
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-07-02 20:39:51 +0530

    issue in links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -180,7 +180,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://www.NavLinkedin.com/in/suraj-goswami01"
+              href="https://www.Linkedin.com/in/suraj-goswami01"
               target="blank"
             >
               Linkedin <FaLinkedin size={30} />
