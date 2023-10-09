0eb6638420e07756b2c8b406a2d2b580c5ee7749
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-10-09 22:41:31 +0530

    Portfolio Updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/SurajG20/Portfolio-Website'
+      className='link footer__link'
+    >
+      Created with ❤️
+    </a>
+  </footer>
+);
+
+export default Footer;
